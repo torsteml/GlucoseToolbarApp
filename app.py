4064485b2e68caa2b9159ea328fe1c3c01eb0be6
Torstein Langan <torstein.langan@bekk.no>
2022-09-08 09:06:56 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from pydexcom import Dexcom
+import rumps
+import os
+
+
+def get_authenticated_client():
+    return Dexcom(os.getenv("DEXCOM_USERNAME", default=None), os.getenv("DEXCOM_PASSWORD", default=None), ous=True)
+
+
+class GlucoseApp(rumps.App):
+
+    def __init__(self):
+        super(GlucoseApp, self).__init__(name="Glucose")
+
+        self.bg = None
+        self.dexcom_client = get_authenticated_client()
+
+    @rumps.clicked("Force update")
+    @rumps.timer(60)
+    def update_glucose(self, sender):
+        self.bg = self.dexcom_client.get_current_glucose_reading()
+
+    @rumps.timer(1)
+    def display_glucose(self, sender):
+        if self.bg is not None:
+            if self.bg.mmol_l < 14 or self.bg.mmol_l > 3:
+                self.title = f"{self.bg.mmol_l} {self.bg.trend_arrow}"
+            if self.bg.mmol_l >= 14 or self.bg.mmol_l <= 3:
+                self.title = f"{self.bg.mmol_l} {self.bg.trend_arrow}"
+        else:
+            self.title = f"Unable to fetch glucose"
+
+
+if __name__ == '__main__':
+    GlucoseApp().run()
